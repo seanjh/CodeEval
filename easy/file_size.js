da440f6ef55cd5126fda145a335e2d8518d57b5a
Sean Herman <seanherman@gmail.com>
2015-01-28 17:02:00 -0500

    final files

---
+++ easy/file_size.js
@@ -0,0 +1,4 @@
+var fs = require('fs');
+fs.stat(process.argv[2], function(err, stats) {
+  console.log(stats.size);
+});
--- easy/sum_digits.js
+++ easy/sum_digits.js
@@ -2,11 +2,14 @@ fs = require('fs');
 fs.readFile(process.argv[2], function(err, data) {
   if (err) throw err;
   var sum = 0;
-  var vals = data.toString().split('');
-  for (i=0; i<vals.length; i++) {
-    if (Number.isInteger(vals[i])) {
-      sum += Number.parseInt(vals[i]);
+  var values = data.toString().split('');
+  for (i=0; i<values.length; i++) {
+    if (values[i] === '\n') {
+      console.log(sum);
+      sum = 0;
+    } else {
+      // console.log('val: ' + values[i]);
+      sum += Number(values[i]);
     }
   }
-  console.log(sum);
-});
+  });
